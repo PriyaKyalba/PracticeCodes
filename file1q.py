7aa65b19e9faf3197058912c3452b2d55aecaf82
PriyaKyalba <priyankakyalba.im24@bmsce.ac.in>
2025-03-23 23:19:39 +0530

    i/o

---
+++ file1q.py
@@ -0,0 +1,9 @@
+# create a new file "practice.txt" using python add the following data in it:
+# hi everyone
+# we are learning file i/o
+# using java
+# i like programming in java
+
+f = open("practice.txt","w")
+f.write("hi everyone \n we are learning file i/o \n using java \n i like programming in java")
+f.close()
