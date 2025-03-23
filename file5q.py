6419913523d707a2ed45ea4cd075a932554148ce
PriyaKyalba <priyankakyalba.im24@bmsce.ac.in>
2025-03-23 23:55:44 +0530

    i/o

---
+++ file5q.py
@@ -0,0 +1,11 @@
+#from a file containing numbers separated by comma, print the count of even numbes
+count = 0
+with open("practice.txt","r")as f:
+    data = f.read()
+    
+    nums = data.split(",")
+    for val in nums:
+        if(int(val) % 2 == 0):
+            count+=1
+
+print(count)    
