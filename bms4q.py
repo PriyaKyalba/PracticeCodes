f734c8bdd064eeb1818c515ad37d82424ea3eedc
PriyaKyalba <priyankakyalba.im24@bmsce.ac.in>
2025-03-25 20:55:46 +0530

    bmsce

---
+++ bms4q.py
@@ -0,0 +1,7 @@
+#WAP to check whether the given number is odd or even 
+num = int(input("enter any number:"))
+if num%2==0:
+    print("entered number is EVEN")
+else:
+    print("entered number is ODD") 
+      
