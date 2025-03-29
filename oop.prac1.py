2359f448292ab0a4744e3db07973e8aabbb02633
Priyanka Kyalba <priyakyalba@gmail.com>
2025-03-29 22:50:29 +0530

    oops

---
+++ oop.prac1.py
@@ -0,0 +1,7 @@
+# creating class,class name sshould always st with capital alphabet
+class Student:
+    name = "priya"
+#creating object
+s1 = Student() # this parenthesis calls the constructor (__init__) which is default
+print(s1)
+print(s1.name)
